2a73e9e9de03e2a3ea5d7c8b7096a1e2f1730fee
Raheel <raheelam98@gmail.com>
2024-10-12 17:15:55 +0500

    update user-service

--- user_service/app/main.py
+++ user_service/app/main.py
@@ -57,9 +57,11 @@ async def life_span(app: FastAPI):
 # Create FastAPI instance
 app = FastAPI(lifespan=life_span, title='Product API')
 
+
 @app.get('/')
 def root_route():
     return {"Welcome to": "User Service"}
+
 ### ========================= *****  ========================= ###
 
 # Function to retrieve user data from the database
@@ -68,12 +70,14 @@ def get_user_from_db(session: DB_Session):
     statement = select(User)
     # Execute the statement and get the list of users
     user_list = session.exec(statement).all()
+
     # If no users found, raise an HTTPException with status code 404
     if not user_list:
         raise HTTPException(status_code=404, detail="Not Found")
     # Otherwise, return the list of users
     else:
         return user_list
+
 
 # API endpoint to get users
 @app.get('/api/get_user')
@@ -89,6 +93,7 @@ def get_user(session: DB_Session):
 def add_user_into_db(form_data: UserBase, session: Session):
     # Create a new User object using the details provided in form_data
     user = User(**form_data.model_dump())
+
     # Add the user to the session
     session.add(user)
     # Commit the session to save the user to the database
@@ -97,6 +102,7 @@ def add_user_into_db(form_data: UserBase, session: Session):
     session.refresh(user)
     print("New user added:", user)
     return user
+
 
 # POST route to add a new user
 @app.post('/api/add_user')
@@ -113,13 +119,17 @@ def update_user_from_db(selected_id: int, update_form_data: UserUpdateModel, ses
     statement = select(User).where(User.user_id == selected_id)
     # Execute the statement and get the selected user
     selected_user = session.exec(statement).first()
+
     # If the user is not found, raise an HTTPException with status code 404
     if not selected_user:
         raise HTTPException(status_code=404, detail="Not Found")
+    
     # Update the user's details with the data from the form
+    # databse               = form data
     selected_user.user_name = update_form_data.user_name
     selected_user.user_password = update_form_data.user_password
     selected_user.user_address = update_form_data.user_address
+
     # Add the updated user to the session
     session.add(selected_user)
     # Commit the session to save the changes to the database
@@ -127,6 +137,7 @@ def update_user_from_db(selected_id: int, update_form_data: UserUpdateModel, ses
     # Refresh the session to retrieve the updated user data
     session.refresh(selected_user)
     return selected_user
+
 
 @app.put('/api/update_user')
 def update_user(id:int, user_detail: UserUpdateModel, session: DB_Session):
@@ -140,9 +151,11 @@ def update_user(id:int, user_detail: UserUpdateModel, session: DB_Session):
 def delete_user_from_db(delete_id: int, session: DB_Session):
     # Retrieve the user from the database using the given ID
     user = session.get(User, delete_id)
+
     # If the user is not found, raise an HTTPException with status code 404
     if not user:
         raise HTTPException(status_code=404, detail="Not Found")
+    
     # Delete the user from the session
     session.delete(user)
     # Commit the session to save the changes to the database
